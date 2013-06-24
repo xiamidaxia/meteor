9f5ecc685e4c45830a277911095041491e34a006
Slava Kim <slava@meteor.com>
2013-06-24 11:36:28 -0700

    Back to 'mongo mongod' targets.

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -152,14 +152,11 @@ MONGO_FLAGS='--ssl --release --static'
 
 if [ "$MONGO_OS" == "osx" ]; then
     # NOTE: '--64' option breaks the compilation, even it is on by default on x64 mac: https://jira.mongodb.org/browse/SERVER-5575
-    python /usr/local/bin/scons $MONGO_FLAGS -j4 --cpppath /usr/local/Cellar/openssl/1.0.1e/include --libpath /usr/local/Cellar/openssl/1.0.1e/lib all
-    python /usr/local/bin/scons $MONGO_FLAGS -j4 --cpppath /usr/local/Cellar/openssl/1.0.1e/include --libpath /usr/local/Cellar/openssl/1.0.1e/lib --prefix=. install
+    python /usr/local/bin/scons $MONGO_FLAGS -j4 --cpppath /usr/local/Cellar/openssl/1.0.1e/include --libpath /usr/local/Cellar/openssl/1.0.1e/lib mongo mongod
 elif [ "$MONGO_OS" == "linux" ] && [ "$ARCH" == "x86_64" ]; then
-    scons $MONGO_FLAGS --64 -j2 --no-glibc-check --cpppath /usr/include/ --libpath /usr/bin/ --prefix=./ all
-    scons $MONGO_FLAGS --64 -j2 --no-glibc-check --cpppath /usr/include/ --libpath /usr/bin/ --prefix=./ install
+    scons $MONGO_FLAGS --64 -j2 --no-glibc-check --cpppath /usr/include/ --libpath /usr/bin/ --prefix=./ mongo mongod
 elif [ "$MONGO_OS" == "linux" ] && [ "$ARCH" == "i686" ]; then
-    scons $MONGO_FLAGS -j2 --no-glibc-check --cpppath /usr/include/ --libpath /usr/bin/ --prefix=./ all
-    scons $MONGO_FLAGS -j2 --no-glibc-check --cpppath /usr/include/ --libpath /usr/bin/ --prefix=./ install
+    scons $MONGO_FLAGS -j2 --no-glibc-check --cpppath /usr/include/ --libpath /usr/bin/ --prefix=./ mongo mongod
 else
     echo "We don't know how to compile mongo for this platform"
     exit 1
